31271c036e356952e0a629c3412e61e6f0409ede
koljs <koljs@users.noreply.github.com>
2026-06-09 21:25:46 +0000

    feat: switch OpenList source to fork repo with GuangYaPan driver
    Changed all references from OpenListTeam/OpenList to koljs/OpenList to use the fork that includes the GuangYaPan (光鸭云盘) driver. Updated in:
    - check_openlist.sh
    - init_openlist.sh
    - build.yaml, build_standalone.yaml, release.yaml, build_openlist.yaml

--- openlist-lib/scripts/init_openlist.sh
+++ openlist-lib/scripts/init_openlist.sh
@@ -6,7 +6,7 @@ TAG_NAME=$(git -c 'versionsort.suffix=-' ls-remote --exit-code --refs --sort='ve
 echo "OpenList - ${TAG_NAME}"
 rm -rf ./src
 unset GIT_WORK_TREE
-git clone --branch "$TAG_NAME" https://github.com/OpenListTeam/OpenList.git ./src
+git clone --branch "$TAG_NAME" https://github.com/koljs/OpenList.git ./src
 rm -rf ./src/.git
 
 echo "Checking cloned source structure:"
