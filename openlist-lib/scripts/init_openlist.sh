80b65a9245f7251217031d4679e8a8cf0f7dbba3
koljs <koljs@users.noreply.github.com>
2026-06-09 21:53:25 +0000

    fix: clone OpenList from main branch instead of tag
    The fork repo only has a 'beta' tag which doesn't include the GuangYaPan driver. Changed init_openlist.sh to clone from main branch directly, and updated all workflow version extraction to get commit hash from main branch instead of tag.

--- openlist-lib/scripts/init_openlist.sh
+++ openlist-lib/scripts/init_openlist.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 GIT_REPO="https://github.com/koljs/OpenList.git"
-TAG_NAME=$(git -c 'versionsort.suffix=-' ls-remote --exit-code --refs --sort='version:refname' --tags $GIT_REPO | tail -n 1 | cut -d'/' -f3)
+BRANCH="main"
 
-echo "OpenList - ${TAG_NAME}"
+echo "OpenList - branch ${BRANCH}"
 rm -rf ./src
 unset GIT_WORK_TREE
-git clone --branch "$TAG_NAME" https://github.com/koljs/OpenList.git ./src
+git clone --branch "$BRANCH" --depth 1 https://github.com/koljs/OpenList.git ./src
 rm -rf ./src/.git
 
 echo "Checking cloned source structure:"
